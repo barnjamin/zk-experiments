abf1a451bf0112a7c6e3071ba2ffd82ecf6664ab
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-12-28 06:10:50 -0500

    tmp

--- groth16/contracts/main.py
+++ groth16/contracts/main.py
@@ -11,6 +11,7 @@ def demo(app_id: int = 0):
     v = Verifier(version=9)
     ac = client.ApplicationClient(algod_client, v, app_id=app_id, signer=acct.signer)
 
+
     if app_id == 0:
         app_id, _, _ = ac.create()
         print(f"Created app: {app_id}")
@@ -19,10 +20,14 @@ def demo(app_id: int = 0):
         ac.build()
         ac.update()
 
-    ac.call(v.bootstrap, vk=get_vk(), boxes=[(0, b"vk")])
+    boxes = [(0, v._vk_box_name.encode())]
 
+    # Bootstrap with vk
+    ac.call(v.bootstrap, vk=get_vk(), boxes=boxes)
+
+    # Pass proof && inputs to be verified
     proof, inputs = get_proof_and_inputs()
-    result = ac.call(v.verify, inputs=inputs, proof=proof, boxes=[(0, b"vk")])
+    result = ac.call(v.verify, inputs=inputs, proof=proof, boxes=boxes)
     print(f"Contract verified? {result.return_value}")
 
 
