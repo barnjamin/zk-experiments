67f154c5cdbdf24004bcb6c817a5c3350ff2e59e
Hang Su <hang.su@algorand.com>
2023-01-05 14:38:56 -0500

    specifying curve

--- circuits/tests/test_merkle.sh
+++ circuits/tests/test_merkle.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-zokrates compile -i test_merkle.zok
+CURVE=bls12_381
+
+zokrates compile -i test_merkle.zok -c $CURVE
 zokrates setup
 zokrates compute-witness -a 3663108286 398046313 1647531929 2006957770 2363872401 3235013187 3137272298 406301144 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
 zokrates generate-proof
-zokrates export-verifier
 zokrates verify
 
 rm abi.json
@@ -15,4 +16,3 @@ rm proof.json
 rm proving.key
 rm verification.key
 rm witness
-rm verifier.sol
