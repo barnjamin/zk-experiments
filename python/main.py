95eb695edd0d5d4574f27604669893a4dc01adc3
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-01-06 10:39:01 -0500

    data merkle root correct

--- python/main.py
+++ python/main.py
@@ -4,6 +4,14 @@ from method import Method
 from consts import QUERIES, INV_RATE, MIN_CYCLES_PO2
 
 
+CIRCUIT_OUTPUT_SIZE = 18
+CIRCUIT_MIX_SIZE = 36
+
+CODE_TAP_SIZE = 15
+DATA_TAP_SIZE = 212
+ACCUM_TAP_SIZE = 36
+
+
 def main():
     with open("../trivial.seal", "rb") as f:
         seal = list(f.read())
@@ -11,22 +19,21 @@ def main():
     with open("../trivial.method", "rb") as f:
         method = Method.from_bytes(f.read())
 
-    for x in method.table:
-        print(x.hex())
-        print()
-
-    circuit_output_size = 18
-    circuit_mix_size = 36
-
-    iop = ReadIOP(circuit_output_size, seal)
+    iop = ReadIOP(CIRCUIT_OUTPUT_SIZE, seal)
 
     po2 = iop.po2
     size = 1 << po2
     domain = INV_RATE * size
-    code_size = 15
 
-    code_merkle = MerkleVerifier(iop, domain, code_size, QUERIES)
+    code_merkle = MerkleVerifier(iop, domain, CODE_TAP_SIZE, QUERIES)
     check_code_merkle(po2, method, code_merkle.root())
+
+    data_merkle = MerkleVerifier(iop, domain, DATA_TAP_SIZE, QUERIES)
+    print(data_merkle.root().hex())
+
+    # TODO: adapter.accumulate?
+
+    # accum_merkle = MerkleVerifier(iop, domain, ACCUM_TAP_SIZE, QUERIES)
 
 
 def check_code_merkle(po2: int, method: Method, merkle_root: bytes) -> bool:
